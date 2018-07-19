72d9dab0fe5d1d263d653b67534c8773890df733
Code <Code@batc.local>
2018-07-19 09:40:18 -0600

    built first functional component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Bottega Madlibs</h1>
+            <p>dklfja;sdfh jas;kdfjkas as;dkfjj;asfkjl ja;lksdfj  a;slkdf;jasf;lkjsa;dfljasdf</p>
+        </div>
+    )
+}
+
+export default Header;
